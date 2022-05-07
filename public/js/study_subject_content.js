f8d0d5ac396eff69a62a4831dc608f97999a3435
whsong92 <ww_song@naver.com>
2022-05-07 15:51:39 +0000

    dddddd

--- public/js/study_subject_content.js
+++ public/js/study_subject_content.js
@@ -71,7 +71,7 @@ var studyContentLib = (function(){
             
         });
 
-        swhCommon.sleep(85).then(
+        swhCommon.sleep(100).then(
             (function(){
                 if(list.length > 0){
                     requestContent(list);
