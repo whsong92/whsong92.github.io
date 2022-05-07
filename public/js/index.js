ef31cd25425345adfc12d9d766f74f564026f422
whsong92 <ww_song@naver.com>
2022-05-07 14:10:47 +0000

    모바일 접근 제한

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,29 @@
 document.addEventListener("DOMContentLoaded", function(){
-    addEvent();
+    init();
 });
 
+function init(){
+    var device = "desktop";
+    const ua = navigator.userAgent;
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+        device = "tablet";
+    }
+    else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+        device = "mobile";
+    }else{
+        device = "desktop";
+    }
+
+    switch(device){
+        case "desktop" :
+            addEvent();
+            break;
+        case "tablet":
+        case "mobile":
+            goMobile();
+            break;
+    }
+}
 
 function addEvent(){
     const lBtn = document.querySelector(".left-btn");
@@ -14,4 +36,9 @@ function addEvent(){
     rBtn.addEventListener("click", function(){
         location.href="./public/pages/main.html?page=project";
     });
+}
+
+function goMobile(){
+    alert("모바일 페이지는 준비 중 입니다.\n구글로 이동합니다.");
+    location.href= "http://google.co.kr";
 }
